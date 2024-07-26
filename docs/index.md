fbbffdb10106dbd1c067cc9a7332d3c234b72b91
Joel Filho <joelfilho02812@gmail.com>
2024-07-26 15:57:35 -0300

    chore: configs & add about page

---
+++ docs/index.md
@@ -0,0 +1,14 @@
+---
+sidebar_position: 1
+---
+
+# Sobre o Blog
+
+O blog é um local de **compartilhamento** de conhecimento sobre as tecnologias e conceitos que eu mesmo estou estudando.
+
+## Learning Paths
+
+Seguirei alguns Learning Paths para me guiar nessa jornada.
+
+- [Learning Path Backend](https://joelbrs.github.io/blog/learning-path-backend)
+- [Learning Path Servless](https://joelbrs.github.io/blog/learning-path-servless)
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,127 +1,97 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
+  title: "Joel's Blog",
+  tagline:
+    "Um blog criado para compartilhar os conhecimentos que estou estudando.",
+  favicon: "img/favicon.ico",
+  url: "http://joelbrs.github.io",
+  baseUrl: "/blog",
+  organizationName: "joelbrs", // Usually your GitHub org/user name.
+  projectName: "blog", // Usually your repo name.
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "pt-BR",
+    locales: ["pt-BR"],
   },
-
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
+          routeBasePath: "/",
+          path: "./docs",
+          sidebarPath: require.resolve("./sidebars.ts"),
+          editUrl: ({ versionDocsDirPath, docPath }) => {
+            return `https://github.com/joelbrs/blog/edit/main/${versionDocsDirPath}/${docPath}`;
+          },
+          editCurrentVersion: true,
+          remarkPlugins: [require("mdx-mermaid")],
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        blog: false,
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: "Joel's Blog",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: "Joel Filho",
+        src: "img/logo.jpg",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
+          href: "https://twitter.com/joelbrs__",
+          position: "right",
+          label: "🐦",
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/joelbrs",
+          position: "right",
+          label: "🐙",
+        },
+        {
+          href: "https://github.com/joelbrs/blog",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
       links: [
         {
-          title: 'Docs',
+          title: "Comunidades",
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: "Linkedin",
+              href: "https://linkedin.com/in/joelbrs",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/joelbrs__",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Outros",
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              label: "GitHub",
+              href: "https://github.com/joelbrs",
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Joel's Blog, Inc.`,
     },
     prism: {
       theme: prismThemes.github,
