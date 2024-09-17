6237acbc892ab9ff89bfdce09552cf49b0d00bd2
Joel da Silva Pereira Filho <joel.pereira@d2ti.com.br>
2024-09-17 10:06:17 -0300

    Updates

--- en-US/assets/js/a5035a8d.c809633d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[1636],{7613:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/studies/en-US/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
